96b2be65d8ad0424dc2a15a9e79a2f7c33c0561f
Jacob Spears <spearsjr@SPEARSJR-W541.rose-hulman.edu>
2019-04-26 13:03:09 -0400

    d4 testing and reports

---
+++ carpentry/d3-mortality-carpentry.R
@@ -0,0 +1,53 @@
+library("tidyverse")
+library("dplyr")
+library("ggplot2")
+library("graphclassmate")
+library("magick")
+
+mortality<- read_csv("data-raw/NCHS_-_Death_rates_and_life_expectancy_at_birth.csv") 
+glimpse(mortality)
+
+mortality <- mortality %>% 
+  rename(
+    Life_Expectancy = "Average Life Expectancy (Years)",
+  ) %>%
+  glimpse()
+
+mortality <- as_tibble(mortality)
+mortality <- filter(mortality,Sex == "Both Sexes")
+
+
+hospice_graph <- ggplot(mortality, aes(x = Year, y = Life_Expectancy, color = Race)) +
+  geom_jitter() +
+  geom_point() +
+  theme_graphclass(line_color = rcb("mid_Gray"), 
+                   font_size = 12) + 
+  scale_color_grey(start=0.7, end=0.2)+
+  theme(axis.line = element_line(colour = rcb("pale_Gray")), 
+        strip.text = element_text(color = rcb("dark_Gray"), face = "bold"), 
+        plot.margin = unit(c(2, 4, 1, 0), "mm"), # top, right, bottom, and left margins
+        panel.border = element_rect(color = rcb("pale_Gray"), fill = NA), 
+        panel.spacing = unit(3, "mm"), 
+        plot.background = element_rect(color = NA, fill = rcb("pale_Gray")), 
+        panel.background = element_rect(color = NA, fill = rcb("pale_Gray")), 
+        panel.grid.minor = element_blank(), 
+        strip.background = element_rect(color = rcb("pale_Gray"), fill = rcb("pale_Gray"))
+  )
+
+image_file <- "resources/hospice.jpg"
+image_url  <- "https://www.lakecountyhospice.org/wp-content/uploads/2014/02/Dying-at-Home-600x399.jpg"
+
+if(!file.exists(image_file)) {
+  hospice <- image_read(image_url)  
+  image_write(hospice, path = image_file, format = "jpg")
+}
+hospice <- image_read("resources/hospice.png")
+hospice <- image_quantize(hospice,  max = 10, colorspace = "gray")
+hospice  <- image_colorize(hospice,  opacity = 25, color = "white")
+
+ggsave(plot = hospice_graph, 
+       filename = "hospice_graph_d4.png",
+       path    = "/figures",
+       width   = 4,
+       height  = 4,
+)
--- explore/crime-data.R
+++ explore/crime-data.R
@@ -12,23 +12,42 @@ crime<- read_csv("data-raw/Crime_Data_from_2010_to_Present.csv")
 crime <- crime %>% 
   as_tibble() %>% 
   glimpse()
+
 crime<-select(crime,'Area ID','Victim Descent','Area Name')
 crime$`Area ID` <- as.numeric(crime$`Area ID`)
 crime <- crime[complete.cases(crime), ]
+
 colnames(crime)[3] <- "Area"
 colnames(crime)[2] <- "Race"
+
 hold <- crime %>% group_by(Area,Race) %>% mutate(arrests = n())
 hold <- ungroup(hold)
 crime <- unique(hold)
+
 crime$Race <- as.factor(crime$Race)
 crime$Area <- as.factor(crime$Area)
+
 newlevels <- c("Asian Indian", "Unknown", "White","Vietnamese","Hawaiian", "Samoan","Pacific Islander","Other","Laotian","Korean","Japanese","American Indian","Hispanic","Guamanian","Filipino","Cambodian","Chinese","Black","General Asian","Undisclosed")
 levels(crime$Race) <- rev(newlevels)
+
 crime <- crime %>%
   mutate(Race = fct_reorder(Race, arrests)) %>% 
   mutate(Area = fct_reorder(Area, arrests))
+
 ggplot(data = crime, aes(x = arrests/1000, y = Race)) + 
   geom_point() +
   facet_wrap(vars(Area), ncol = 6, as.table = FALSE) +
   theme_graphclass()
+
 write_rds(crime,"data/crime-data.rds")
+
+library("VIM")
+aggr(crime, 
+     # color: observed, missing, imputed
+     col = c(rcb("pale_BG"), rcb("mid_BG")), 
+     numbers  = TRUE, 
+     bars     = TRUE, 
+     sortVars = TRUE, 
+     prop     = TRUE, 
+     combine  = TRUE
+)
